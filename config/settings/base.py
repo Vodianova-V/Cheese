577942c6ff63ccf422abe21ea1edb04c416d7d8d
Vodianova-V <vladka_v@tut.by>
2021-09-06 11:55:46 +0300

    new setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,6 +39,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
 LOCALE_PATHS = [str(BASE_DIR / "locale")]
 
+WSGI_APPLICATION = "gettingstarted.wsgi.application"
+
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
